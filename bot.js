e8a1709a8b60ce33e8cf08af65c590776fa5ce0d
garrsimm <garrett@twmhou.com>
2016-03-10 15:50:10 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = ("MOTHERFUCKER!");
+  botResponse = ("Fuck utexaspunk!");
 
   options = {
     hostname: 'api.groupme.com',
