77102647ca88c2429754211d31d0ad8ac00db474
garrsimm <garrett@twmhou.com>
2017-01-05 16:30:31 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,5 +53,55 @@ function postMessage() {
   botReq.end(JSON.stringify(body));
 }
 
+function respond() {
+  var request = JSON.parse(this.req.chunks[0]),
+      botRegex = /^K$/;
+
+  if(request.text && botRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage();
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+}
+
+function postMessage() {
+  var botResponse, options, body, botReq;
+
+  botResponse = ("What the fuck did you just say to me you little bitch?");
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
 
 exports.respond = respond;
