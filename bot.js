6e03ecc2134a077bc0c059ce9481b7970ab8e90d
garrsimm <garrett@twmhou.com>
2017-04-11 11:41:16 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = ("BOOMS! proudly sponsored by EadoBikeCo™");
+  botResponse = ("AM I BEING DEPLANED?!™");
 
   options = {
     hostname: 'api.groupme.com',
